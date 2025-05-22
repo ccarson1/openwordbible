ff44cf9a35df796bfc7eb313a578a60d53047177
nathansmith2025 <166193373+nathansmith2025@users.noreply.github.com>
2025-05-22 18:08:52 -0400

    Update urls.py
    Updated urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import dashboard, analytics, settings_view, books, update_publish_status
 
 app_name = "dashboard"
@@ -9,5 +12,7 @@ urlpatterns = [
     path("settings_view/", settings_view, name="settings_view"),
     path("books/", books, name="books"),
     path('books/<int:book_id>/publish/', update_publish_status, name='update_publish_status'),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
